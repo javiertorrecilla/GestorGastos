b306e56d78f61f74305d2b949506f686bd84c105
Miguel Galdeano <99599864+MikiGR@users.noreply.github.com>
2024-12-30 21:31:43 +0100

    Update MainActivity.java

--- GestorGastosPersonales/app/src/main/java/com/example/gestorgastospersonales/MainActivity.java
+++ GestorGastosPersonales/app/src/main/java/com/example/gestorgastospersonales/MainActivity.java
@@ -167,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
 
             String categoryInSpanish = categoryMap.get(expense.getCategoria());
             boolean matchesCategory = selectedCategory.equals(getString(R.string.all_categories)) ||
+             expense.getCategoria().equalsIgnoreCase(selectedCategory)||
                     (categoryInSpanish != null && categoryInSpanish.equalsIgnoreCase(selectedCategory));
 
 
